d110cef280a799676ad899894929ecaf8c6b17f3
Alex Lapinski <contact@alexlapinski.name>
2017-05-16 22:14:25 -0400

    Added more integration with mongodb

--- index.js
+++ index.js
@@ -6,8 +6,25 @@ const PORT = process.env.PORT || 3000;
 
 mongoose.connect('mongodb://mongo:27017');
 
+var Cat = mongoose.model('Cat', {name: String});
+
+var kitty = new Cat({ name: 'Zildjian' });
+kitty.save((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('meow');
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/cats', (req, res) => {
+    Cat.find().exec((err, data) => {
+        res.json(data);
+    });
 });
 
 app.listen(PORT, () => {
