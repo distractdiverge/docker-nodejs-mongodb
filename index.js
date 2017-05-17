ba4b832938537df355629e907a125f4c8bfbca79
Alex Lapinski <contact@alexlapinski.name>
2017-05-16 22:08:06 -0400

    Connected mongo container to nodejs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://mongo:27017');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
